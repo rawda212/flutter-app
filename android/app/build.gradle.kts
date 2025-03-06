23942990a2df5029d5579e9efb83add81cdd72c8
rawda212 <rawda.saleh202@gmail.com>
2025-03-06 02:30:34 +0200

    First commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.pro"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34  // تحديد رقم الإصدار مباشرةً بدلًا من flutter.compileSdkVersion
     ndkVersion = "29.0.13113456"
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,24 +20,20 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.pro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21  // حددي الحد الأدنى المطلوب حسب الحاجة
+        targetSdk = 34  // استخدمي أحدث إصدار مدعوم من Flutter وAndroid
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
+
 
 flutter {
     source = "../.."
